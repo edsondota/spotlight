446854af744320faf9423ad6bb066e8c2a0ac371
Edson Dota <edsondota@gmail.com>
2019-10-06 14:15:24 -0300

    Add Search component

---
+++ src/components/Search.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <header>
+      <h1>
+        Spotlight
+        <small>Upcoming Movies list</small>
+      </h1>
+    </header>
+    <section>
+      <input type="text">
+      <button>Search</button>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+};
+</script>
+
+<style lang="scss" scoped>
+  header {
+    height: 60px;
+    background-color: black;
+    color: white;
+
+    h1 {
+      padding: 10px;
+      small {
+        font-size: .8em;
+        font-weight: normal;
+      }
+    }
+  }
+</style>
