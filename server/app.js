8e0aaa99430771e9bc131b780c51a15200f0b476
Edson Dota <edsondota@gmail.com>
2019-10-08 23:06:42 -0300

    Serve dist on expressjs

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
-const app = require('express')();
+const express = require('express');
 const expressGraphQL = require('express-graphql');
 const cors = require('cors');
+const path = require('path');
 const schema = require('./schema');
 const resolvers = require('./resolvers');
 
 require('dotenv').config();
 
+const app = express();
+
 app.use(cors());
 
 resolvers.cacheResults();
+
+app.use('/', express.static(path.join(__dirname, '../dist')));
 
 app.use('/graphql', expressGraphQL({
   schema,
